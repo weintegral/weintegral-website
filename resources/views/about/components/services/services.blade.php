20b210832a92ffb3a4985b692b1fbf62521c3992
Gopibabu Srungavarapu <gsrungavarapu@tmhcc.com>
2023-04-23 23:58:17 -0400

    👌 IMPROVE: updated services

--- resources/views/about/components/services/services.blade.php
+++ resources/views/about/components/services/services.blade.php
@@ -6,48 +6,48 @@
             <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
                 <div class="icon-box">
                     <div class="icon"><i class="bx bxl-dribbble"></i></div>
-                    <h4><a href="">Lorem Ipsum</a></h4>
-                    <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                    <h4><a href="">Application Development</a></h4>
+                    <p>WeIntegral as a pioneer IT application (app) development company effectively helps you to meet your technology-specific objectives.</p>
                 </div>
             </div>
 
             <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
                 <div class="icon-box">
                     <div class="icon"><i class="bx bx-file"></i></div>
-                    <h4><a href="">Sed ut perspiciatis</a></h4>
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+                    <h4><a href="">Quality Testing</a></h4>
+                    <p>Our software quality assurance services focus on software deployment readiness ensuring better quality enhanced performance.</p>
                 </div>
             </div>
 
             <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
                 <div class="icon-box">
                     <div class="icon"><i class="bx bx-tachometer"></i></div>
-                    <h4><a href="">Magni Dolores</a></h4>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+                    <h4><a href="">Maintenance Support</a></h4>
+                    <p>WeIntegral, Inc. has worked with some of the best banks worldwide, providing them mission-critical solutions and services.</p>
                 </div>
             </div>
 
             <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
                 <div class="icon-box">
                     <div class="icon"><i class="bx bx-world"></i></div>
-                    <h4><a href="">Nemo Enim</a></h4>
-                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+                    <h4><a href="">Technology</a></h4>
+                    <p>Providing Customized Classroom and Online Training to students, corporates in various software technologies.</p>
                 </div>
             </div>
 
             <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
                 <div class="icon-box">
                     <div class="icon"><i class="bx bx-slideshow"></i></div>
-                    <h4><a href="">Dele cardo</a></h4>
-                    <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
+                    <h4><a href="">Consulting</a></h4>
+                    <p>WeIntegral IT Consulting services facilitate customers in process and technology-driven transformation to achieve business outcomes.</p>
                 </div>
             </div>
 
             <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
                 <div class="icon-box">
                     <div class="icon"><i class="bx bx-arch"></i></div>
-                    <h4><a href="">Divera don</a></h4>
-                    <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
+                    <h4><a href="">Staffing</a></h4>
+                    <p>WeIntegral offers Staffing Services to IT Multinational organizations in various domains of talent acquisition.</p>
                 </div>
             </div>
 
