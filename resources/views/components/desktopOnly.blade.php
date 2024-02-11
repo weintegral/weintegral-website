2a92d0b6ef7f5dc8e757a4091a3a54903b179ad7
gopibabu srungavarapu <s.gopibabu@gmail.com>
2024-02-10 20:08:37 -0500

    Update add desktopOnly component

---
+++ resources/views/components/desktopOnly.blade.php
@@ -0,0 +1,3 @@
+<div class="container alert alert-primary text-center" role="alert">
+    ⚠️ Please open this page in desktop browser only for better viewing experience.
+</div>
--- resources/views/management/marketingData.blade.php
+++ resources/views/management/marketingData.blade.php
@@ -5,26 +5,18 @@
 @section('content')
     <main id="main">
         @include('management.components.marketing.breadcrumbs')
-
+        @include('components.desktopOnly')
         <div class="container">
             <h2>Submission Data</h2>
             <div id="marketing-submission-data">
                 <iframe 
-                    width="600" 
-                    height="500"  
-                    frameborder="0" 
-                    scrolling="no" 
-                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQD--45vQupz7V4wLSps4ZKVrOzFjDO5PQIVxMo9SIhIuiLadq3EuY-vk_x7Vu5RcoCNx_UDFdM7fEk/pubchart?oid=1328419568&amp;format=interactive"
-                    >
-                </iframe>
-                
-                <iframe 
-                    width="100%"
-                    height="800px"
-                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQD--45vQupz7V4wLSps4ZKVrOzFjDO5PQIVxMo9SIhIuiLadq3EuY-vk_x7Vu5RcoCNx_UDFdM7fEk/pubhtml?gid=588945301&amp;single=true&amp;widget=true&amp;headers=false"
-                    style="display: none;"
-                    >
-                </iframe>
+                width="600" 
+                height="500"  
+                frameborder="0" 
+                scrolling="no" 
+                src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQD--45vQupz7V4wLSps4ZKVrOzFjDO5PQIVxMo9SIhIuiLadq3EuY-vk_x7Vu5RcoCNx_UDFdM7fEk/pubchart?oid=1328419568&amp;format=interactive"
+                >
+            </iframe>
             </div>
         </div>
         
