fa30ce39ae62fbdf4710dc74d1055c3bce8a5c5e
Gopibabu Srungavarapu <s.gopibabu@gmail.com>
2024-09-23 12:06:06 -0400

    updated api documentation

---
+++ app/Providers/SwaggerUiServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class SwaggerUiServiceProvider extends ServiceProvider
+{
+    public function boot() : void
+    {
+        Gate::define('viewSwaggerUI', function ($user = null) {
+            return true;
+        });
+    }
+}
--- public/index.php
+++ public/index.php
@@ -35,6 +35,19 @@ require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
+| Run Swagger Doc Generator
+|--------------------------------------------------------------------------
+|
+| It will generate the OpenAPI documentation for the application.
+|
+*/
+//$openapi = \OpenApi\Generator::scan([__DIR__.'/../app/Http/Controllers']);
+//
+//header('Content-Type: application/json');
+//echo $openapi->toJson();
+
+/*
+|--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
 |
