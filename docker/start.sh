b522662288d9629debb90387e0ffcf898887bd79
Gopibabu Srungavarapu <s.gopibabu@gmail.com>
2024-10-01 12:24:16 -0400

    added supervisor to run scheduler jobs

--- docker/start.sh
+++ docker/start.sh
@@ -2,15 +2,4 @@
 
 set -e
 
-env=${APP_ENV:-production}
-role=${CONTAINER_ROLE:-app}
-
-echo "The environment is $env"
-
-if [ "$role" = "app" ]; then
-    echo "Container role \"$role\""
-else
-    echo "Container role \"$role\""
-fi
-
 exec apache2-foreground
